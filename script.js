fdd2abd64b07223c9e97b3d50a03bfd1245a2e08
Mohit Pohwani <pohwanimohit@gmail.com>
2025-06-12 19:17:42 +0530

    clone insights

---
+++ script.js
@@ -0,0 +1,6 @@
+ document.querySelector(".hamburger").addEventListener("click", () => {
+        document.querySelector(".left").style.left = "0";
+    })
+    document.querySelector(".cross").addEventListener("click", () => {
+        document.querySelector(".left").style.left = "-120%";
+    })
